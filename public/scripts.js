d344e34002aa90dba3e501e6f4a08fc4aad6fc60
sleeplesseditor <josh@bryson.me.uk>
2022-01-24 00:19:50 +0000

    Join namespace and room functionality expanded

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,10 @@
-const socket = io('http://localhost:9000');
-
-socket.on('connect', () => {
-    console.log(socket.id)
+const username = prompt('What is your username?')
+const socket = io('http://localhost:9000', {
+    query: {
+        username
+    }
 });
+let nsSocket = "";
 
 socket.on('nsList', (nsData) => {
     let namespacesDiv = document.querySelector('.namespaces');
@@ -13,26 +15,8 @@ socket.on('nsList', (nsData) => {
     Array.from(document.getElementsByClassName('.namespace')).forEach((element) => {
         element.addEventListener('click', (e) => {
             const nsEndpoint = element.getAttribute('ns');
-
+            joinNs(nsEndpoint);
         });
     });
-    const nsSocket = io('http://localhost:9000/wiki');
-    nsSocket.on('nsRoomLoad', (nsRooms) => {
-        
-    });
-});
-
-socket.on('messageFromServer', (dataFromServer) => {
-    console.log(dataFromServer);
-    socket.emit('messageToServer', ({data: 'This is from the client'}))
-});
-
-document.querySelector('#message-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const newMessage = document.querySelector('#user-message').value;
-    socket.emit('newMessageToServer', { text: newMessage });
-});
-
-socket.on('messageToClients', (msg) => {
-    console.log(msg);
+    joinNs('/wiki');
 });
--- slack.js
+++ slack.js
@@ -21,7 +21,42 @@ io.on('connection', (socket) => {
 
 namespaces.forEach((namespace) => {
     io.of(namespace.endpoint).on('connection', (nsSocket) => {
-        console.log(`${socket.id} has joined ${namespace.endpoint}`);
-        nsSocket.emit('nsRoomLoad', namespaces[0].rooms)
+        const userName = nsSocket.handshake.query.username;
+        nsSocket.emit('nsRoomLoad', namespaces.rooms)
+        nsSocket.on('joinRoom', (roomToJoin, numOfUsersCallback) => {
+            const roomToLeave = Object.keys(nsSocket.rooms)[1];
+            nsSocket.leave(roomToLeave);
+            updateUsersInRoom(namespace, roomToLeave);
+            nsSocket.join(roomToJoin);
+            // io.of('/wiki').in(roomToJoin).client((error, client) => {
+            //     numOfUsersCallback(client.length);
+            // })
+            const nsRoom = namespace.rooms.find((room) => {
+                return room.roomtTitle === roomToJoin;
+            });
+            nsSocket.emit('historyCatchUp', nsRoom.history);
+            updateUsersInRoom(namespace, roomToJoin);
+        });
+        nsSocket.on('newMessageToServer', (msg) => {
+            const fullMessage = {
+                text: msg.text,
+                time: Date.now(),
+                username: userName,
+                avatar: 'https://via.placeholder.com/30'
+            }
+            
+            const roomTitle = Object.keys(nsSocket.rooms)[1];
+            const nsRoom = namespace.rooms.find((room) => {
+                return room.roomtTitle === roomTitle;
+            });
+            nsRoom.addMessage(fullMessage);
+            io.of(namespace.endpoint).to(roomTitle).emit('messageToClients', fullMessage);
+        });
     });
 });
+
+function updateUsersInRoom(namespace, roomToJoin) {
+    io.of(namespace.endpoint).in(roomToJoin).clients((error, clients) => {
+        io.of(namespace.endpoint).in(roomToJoin).emit('updateMembers', clients.length)
+    });
+}
